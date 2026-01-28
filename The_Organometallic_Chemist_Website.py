77cae862a2c0a026b82c1efb9cb9ada581cb88e9
baloyit-rgb <baloyit586@gmail.com>
2026-01-28 21:00:02 +0200

    Add files via upload

---
+++ The_Organometallic_Chemist_Website.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 12:02:39 2026
+
+@author: baloy
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("The Organometallic Chemist")
+
+name = "Tinyiko Baloyi"
+Field = "Chemistry"
+Institution = "University of Johannesburg"
+
+st.image(
+    "spheres-5806920_640.jpg")
+st.header("About me")
+st.write(" I am Tinyiko Baloyi and a MSc Chemistry student at the University of Johannesburg. I am Fun outgoing(when forced....I love my own space)")
+
+st.header("More about me") 
+st.write("Passionate about what I do, which is currently carbon dioxide utilization, and very much open to learning new skills to better my work")
+st.write("This website is proof of that")
+
+st.header("Publications")
+st.write("I have no publications, but we working on that :wink:")
+
+st.header("Contact information")
+st.write("Email - baloyit586@gmail.com")
