d8e244e0f5e0c2dce9004f374152784bab86627d
baloyit-rgb <baloyit586@gmail.com>
2026-01-30 13:29:01 +0200

    Add files via upload

--- The_Organometallic_Chemist_Website.py
+++ The_Organometallic_Chemist_Website.py
@@ -14,12 +14,11 @@ st.title("The Organometallic Chemist")
 name = "Tinyiko Baloyi"
 Field = "Chemistry"
 Institution = "University of Johannesburg"
-st.image("https://github.com/baloyit-rgb/CSS_Streamlit_2026/blob/main/spheres-5806920_640.jpg?raw=true")
+
+st.image(
+    "spheres-5806920_640.jpg")
 st.header("About me")
-st.write(" I am Tinyiko Baloyi and a MSc Chemistry student at the University of Johannesburg. I am a young dynamic chemistry researcher, with growing interests in green chemistry. Passionate about science education, committed to empowering 
-learners through innovative and learner-centered teaching, and eager to expand my 
-knowledge in various fields.") 
-st.write("Fun fact: I am Fun outgoing(when forced....I love my own space)")
+st.write(" I am Tinyiko Baloyi and a MSc Chemistry student at the University of Johannesburg.I am a young dynamic chemistry researcher, with growing interests in green chemistry. Passionate about science education, committed to empowering learners through innovative and learner-centered teaching. I am Fun outgoing(when forced....I love my own space)")
 
 st.header("More about me") 
 st.write("Passionate about what I do, which is currently carbon dioxide utilization, and very much open to learning new skills to better my work")
@@ -29,7 +28,4 @@ st.header("Publications")
 st.write("I have no publications, but we working on that :wink:")
 
 st.header("Contact information")
-
 st.write("Email - baloyit586@gmail.com")
-
-
